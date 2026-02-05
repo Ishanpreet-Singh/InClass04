cec950873f430d3b50bedda00f1d28239c0340bc
Ishanpreet-Singh <sin14799@sheridancollege.ca>
2026-02-05 17:56:48 -0500

    initial comment

---
+++ src/projectishan/ProjectIshan.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectishan;
+
+/**
+ *
+ * @author ishan
+ */
+public class ProjectIshan {
+
+    public double add(double a, double b)
+    {
+        return a*b;
+    }
+    
+}
